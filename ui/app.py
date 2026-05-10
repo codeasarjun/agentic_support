45ab6e79d6c03cea3591ee07ba4494c59b08849e
Arjun Raj <codeasarjun@gmail.com>
2026-05-10 15:20:14 +0530

    basic ui added

---
+++ ui/app.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+
+from graph.workflow import workflow
+
+st.set_page_config(
+    page_title="Customer Support AI",
+    layout="centered"
+)
+
+st.title("Multi-Agent Customer Support System")
+
+ticket = st.text_area(
+    "Enter Customer Ticket"
+)
+
+if st.button("Submit Ticket"):
+
+    result = workflow.invoke({
+        "ticket": ticket
+    })
+
+    st.subheader("Category")
+    st.write(result["category"])
+
+    st.subheader("Sentiment")
+    st.write(result["sentiment"])
+
+    st.subheader("Escalation")
+    st.write(result["escalation"])
+
+    st.subheader("AI Response")
+    st.write(result["response"])
